7c930b13322b4a500449a19bab937ac8a2ac3ca6
RavenFromTheDump <osuname848@gmail.com>
2023-03-05 13:43:36 +0100

    movie component added

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
+import MainPage from './pages/main-page';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
